3f57f61c221b892ad6e4183da770d1d13dc1f007
ClassicMan <legenderyprime@gmail.com>
2025-10-15 17:26:42 +0100

    Removed a declared variable to avoid conflict

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ pokemonName.addEventListener('keydown', (e) => {
   if(e.key === 'Enter') {
     fetchData();
   }
-})
+});
 
 async function fetchData() {
       const pokemonName = document.getElementById('pokemonName').value.toLowerCase();
@@ -22,8 +22,7 @@ async function fetchData() {
       const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
       if(!response.ok) {
         if(response.status === 404) {
-              const pokemonName = document.getElementById('pokemonName').value;
-              alert(`No Pokémon with the name '${pokemonName}' was found. Please try another.`)
+              alert(`No Pokémon with the name '${pokemonName}' was found. Please try another.`);
           }
           throw new Error(`Pokémon not found! Please check the name and try again.`);
           
@@ -31,7 +30,7 @@ async function fetchData() {
           const data = await response.json();
           console.log(data);
           if(pokemonName === '') {
-            alert('Please enter a Pokémon name')
+            alert('Please enter a Pokémon name');
           } else {
             pokeImg.src = data.sprites.front_default;
             pokeImg.classList.add('show');
@@ -39,10 +38,10 @@ async function fetchData() {
             pkmnType.textContent = data.types.map(item => item.type.name).join(', ').toUpperCase();
             pkmnAbility.textContent = data.abilities.map(item => item.ability.name).join(', ').toUpperCase();
           }
-      }
+      };
       
   } catch(error) {
       console.error(error);
   }
-}
+};
 
