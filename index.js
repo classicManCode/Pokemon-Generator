77d5b64aebb997566741df7e878b73a052fd6562
ClassicMan <legenderyprime@gmail.com>
2025-10-15 16:05:34 +0100

    Added an if statement to check if the user input is available in the API

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const getPokemon = document.getElementById('getPokemon');
 
 async function fetchData() {
+      const pokemonName = document.getElementById('pokemonName').value.toLowerCase;
+      const pokeImg = document.getElementById('pokeImg');
+      const pkmnName = document.getElementById('pkmnName');
+      const pkmnType = document.getElementById('pkmnType');
+      const pkmnAbility = document.getElementById('pkmnAbility');
+
   try{
-      const pokemonName = document.getElementById('pokemonName');
+
       const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
       if(!response.ok) {
           throw new Error(`Pokémon not found! Please check the name and try again.`);
+          if(response.status === 404) {
+              alert(`No Pokémon with the name '${pokemonName}' was found. Please try another.`)
+          }
       } else {
           const data = await response.json();
           console.log(data);
+          if(pokemonName === '') {
+            alert()
+          }
       }
       
   } catch(error) {
